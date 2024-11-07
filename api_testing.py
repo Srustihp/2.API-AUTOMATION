00a008010b55238ae71c169aa69b7f75d9b997ca
Srustihp <134717108+Srustihp@users.noreply.github.com>
2024-11-08 05:10:36 +0530

    Update api_testing.py

--- api_testing.py
+++ api_testing.py
@@ -1,6 +1,5 @@
 import requests
 url = "https://reqres.in/api/users?page=2"
-#url = "https:hvkabch.com"
 response = requests.get(url)
 if response.status_code == 200:
     print("Test Passed: Status code is 200")
